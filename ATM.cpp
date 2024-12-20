2e17820baf44d988986b8c028efdbeb9cb6790ea
unknown <moazqz47@gmail.com>
2024-12-20 23:09:28 +0200

    Added ChangePIN() method

--- ATM.cpp
+++ ATM.cpp
@@ -27,16 +27,50 @@ public:
     void withdraw(long b) {
     balance -= b;
     cout << "Your NEW account balance is: " << balance << endl;
+    }
 
-}
+    void changePIN()
+    {
+        int tmp_currentPass;
+        cout << "Please enter the Current PIN: ";
+        cin >> tmp_currentPass;
+
+        if (tmp_currentPass == pin)
+        {
+            int tmp_newPass1, tmp_newPass2;
+            cout << "Please enter the New PIN: ";
+            cin >> tmp_newPass1;
+            cout << "Please enter the New PIN Again: ";
+            cin >> tmp_newPass2;
+
+            if (tmp_newPass1 == tmp_newPass2)
+            {
+                pin = tmp_newPass1; // Assign the new PIN to the member variable
+                cout << "PIN changed successfully." << endl;
+            }
+            else
+            {
+                cout << "New PINs do not match. Please try again." << endl;
+            }
+        }
+        else
+        {
+            cout << "Current PIN is incorrect." << endl;
+        }
+    }
+
+
+
+
+
 };
 
 
 
 int main()
 {
-    Account account1(253, 2343, 500000);
+    Account account1(253, 1234, 500000);
     account1.checkBalance();
-    account1.deposit(300);
+    account1.changePIN();
 
 }
